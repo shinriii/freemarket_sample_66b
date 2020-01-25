9d9a6fe5e2d6e2cdfd19a1408850e7db27923c1a
shinriii <q9s5z20o.1i@ezweb.ne.jp>
2020-01-25 15:01:24 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_01_20_145413) do
-
 ActiveRecord::Schema.define(version: 2020_01_22_083004) do
-
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,14 +39,11 @@ ActiveRecord::Schema.define(version: 2020_01_22_083004) do
     t.string "address_kana_firstname", null: false
     t.string "address_kana_lastname", null: false
     t.string "zipcode", null: false
+    t.string "prefectures", null: false
     t.string "municipalities", null: false
     t.text "address", null: false
     t.string "building"
     t.bigint "phone_number"
-    t.integer "prefecture_id", null: false
-    t.integer "prefecture_id"
-    t.string "city"
-
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -63,7 +56,6 @@ ActiveRecord::Schema.define(version: 2020_01_22_083004) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
@@ -72,7 +64,6 @@ ActiveRecord::Schema.define(version: 2020_01_22_083004) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
