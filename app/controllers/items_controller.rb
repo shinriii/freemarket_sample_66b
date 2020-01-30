4d50de13bfad60037498c59381c5fbc71cd65a8f
shinriii <q9s5z20o.1i@ezweb.ne.jp>
2020-01-30 20:40:24 +0900

    changedkey

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,8 @@ class ItemsController < ApplicationController
   end
 
   require 'payjp'
-  Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_ACCESS_KEY)
   def purchase
-    Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
+    Payjp.api_key = "sk_test_aad84a247a00212e7cedabec"
     @item = Item.find(params[:item_id])
     Payjp::Charge.create(
       amount: @item.price, 
